3a544dbfdf04d103d5048fc1577bcf810abf0d4d
Lucas Schumacher <lucas@aceto.dev>
2023-10-11 10:10:59 +0200

    Allow multiple bot instances

--- StarCraft2Bot/Program.cs
+++ StarCraft2Bot/Program.cs
@@ -1,46 +1,50 @@
 ﻿using SC2APIProtocol;
 using Sharky;
 using Sharky.DefaultBot;
-using Sharky.Helper;
 using StarCraft2Bot.Helper;
 
 namespace StarCraft2Bot
 {
     internal class Program
     {
+        private static int startupPort = 5000;
+
+        public static Task StartSinglePlayerGame()
+        {
+            var gameConnection = new GameConnection();
+            var defaultSharkyBot = new DefaultSharkyBot(gameConnection);
+            defaultSharkyBot.BuildChoices[Race.Terran] = new BuildChoicesManager(defaultSharkyBot).GetBuildChoices();
+
+            var exampleBot = defaultSharkyBot.CreateBot(defaultSharkyBot.Managers, defaultSharkyBot.DebugService);
+            
+            startupPort += 5;
+
+            return gameConnection.RunSinglePlayer(exampleBot, @"InsideAndOutAIE.SC2Map", Race.Terran, Race.Terran, Difficulty.Easy, AIBuild.RandomBuild, startupPort);
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Starting up...");
-            Console.WriteLine("Connecting to game...");
-            // first we need to create a game connection for the SC2 api. The bot uses this to communicate with the game
-            var gameConnection = new GameConnection();
-            // We get a default bot that has everything setup.  You can manually create one instead if you want to more heavily customize it.  
-            var defaultSharkyBot = new DefaultSharkyBot(gameConnection);
 
             Console.WriteLine("Setting up AI...");
             ValueCallbackService.Init(ValueManager.GetValue);
 
-            Console.WriteLine("Loading builds...");
-            BuildChoicesManager.Init(defaultSharkyBot);
+            Console.WriteLine("Starting bot(s)...");
 
-            // we configure the bot with our own builds
-            defaultSharkyBot.BuildChoices[Race.Terran] = BuildChoicesManager.GetBuildChoices();
+            var games = new List<Task>();
+            var instanceCount = 1;
 
-            // we create a bot with the modified default bot we made
-            var exampleBot = defaultSharkyBot.CreateBot(defaultSharkyBot.Managers, defaultSharkyBot.DebugService);
-            
-            var myRace = Race.Terran;
-            if (args.Length == 0)
+            if (args.Length > 0 && args.FirstOrDefault() == "-instances")
             {
-                // if there are no arguments passed we play against a comptuer opponent
-                gameConnection.RunSinglePlayer(exampleBot, @"InsideAndOutAIE.SC2Map", myRace, Race.Terran, Difficulty.Easy, AIBuild.RandomBuild).Wait();
+                instanceCount = Int32.Parse(args[1]);
             }
-            else
+
+            for (int i = 0; i < instanceCount; i++)
             {
-                // when a bot runs on the ladder it will pass arguments for a specific map, enemy, etc.
-                gameConnection.RunLadder(exampleBot, myRace, args).Wait();
+                games.Add(StartSinglePlayerGame());
             }
+
+            Task.WaitAll(games.ToArray());
         }
     }
 }
