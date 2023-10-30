4eb927c51c513ce8e813b8ffbb25a5d76b68fde8
Daniel Motz <info@danielmotz.de>
2023-10-30 19:37:47 +0100

    Add the missing opener steps

--- StarCraft2Bot/Builds/Defensive Standard Opener.cs
+++ StarCraft2Bot/Builds/Defensive Standard Opener.cs
@@ -4,7 +4,6 @@ using Sharky.Chat;
 using Sharky.DefaultBot;
 using Sharky.MicroControllers;
 using Sharky.MicroTasks;
-using Sharky.Proxy;
 using Sharky;
 using StarCraft2Bot.Builds.Base;
 using StarCraft2Bot.Builds.Base.Desires;
@@ -14,83 +13,56 @@ namespace StarCraft2Bot.Builds
 {
     public class DefensiveOpener : Build
     {
-        private readonly ProxyLocationService proxyLocationService;
         private bool openingAttackChatSent;
-        private readonly ProxyTask proxyTask;
 
         private Queue<BuildAction>? BuildOrder { get; set; }
 
         public DefensiveOpener(DefaultSharkyBot defaultSharkyBot, IIndividualMicroController scvMicroController) : base(defaultSharkyBot)
         {
-            proxyLocationService = defaultSharkyBot.ProxyLocationService;
             openingAttackChatSent = false;
-            proxyTask = new ProxyTask(defaultSharkyBot, false, 0.9f, string.Empty, scvMicroController)
-            {
-                ProxyName = nameof(DefensiveOpener)
-            };
         }
-        /**
-            14	  0:18	  Supply Depot	  
-            15	  0:41	  Barracks	  
-            16	  0:45	  Refinery	  
-            16	  0:54	  Refinery	  
-            19	  1:28	  Reaper, Orbital Command	  
-            19	  1:31	  Supply Depot	  
-            20    1:42    Factory	  
-            21	  2:03	  Reaper	  
-            23	  2:21	  Command Center	  
-            24	  2:28	  Hellion
-        
-        Second part: defensive opener
-        (not implemented yet)
-
-  	        2:32	Starport	  //Prioritize this over Reactor, Supply Depot and even SCVs if necessary.
-  	  	            Barracks Reactor	  
-  	  	            Supply Depot	  
-  	  	            Widow Mine	  
-  	  	            Marine x2	  
-  	  	            Factory Tech Lab	  
-  	  	            Starport Tech Lab	  
-  	        3:12	Refinery	  //To make sure the refinery finishes *with* the CC.
-  	  	            Marine x2	
-        **/
 
         public override void StartBuild(int frame)
         {
             base.StartBuild(frame);
 
             BuildOptions.StrictGasCount = true;
-            BuildOptions.StrictSupplyCount = true;
-            BuildOptions.StrictWorkerCount = true;
-
-            MicroTaskData[GetType().Name] = proxyTask;
-            var proxyLocation = proxyLocationService.GetCliffProxyLocation();
-            MacroData.Proxies[proxyTask.ProxyName] = new ProxyData(proxyLocation, MacroData);
-            proxyTask.Enable();
+            // BuildOptions.StrictSupplyCount = true;
+            // BuildOptions.StrictWorkerCount = true;
 
             BuildOrder = new Queue<BuildAction>();
             MacroData.DesiredUnitCounts[UnitTypes.TERRAN_SCV] = 18;
             // supply depot, 14, 0:18
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(14, MacroData), new SupplyDepotDesire(1, MacroData)));
-            // barracks, 15, 0:41
-            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(15, MacroData), new ProxyProductionStructureDesire(UnitTypes.TERRAN_BARRACKS, 1, MacroData, proxyTask.ProxyName)));
-            // 2 refineries, 16, 0:45/0:54
+            // Barrack
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(15, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_BARRACKS, 1, MacroData)));
+            // 1 refinery, 16, 0:45/0:54
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(16, MacroData), new GasBuildingCountDesire(1, MacroData)));
-            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(16, MacroData), new GasBuildingCountDesire(1, MacroData)));
-            // Reaper, 19, 1:28
-            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(19, MacroData), new UnitDesire(UnitTypes.TERRAN_REAPER, 1, MacroData.DesiredUnitCounts)));
-            // Orbital Command, 19, 1:28
+            // Reaper, 19
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(19, MacroData), new UnitDesire(UnitTypes.TERRAN_REAPER, 3, MacroData.DesiredUnitCounts)));
+            // Orbital Command, 19
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(19, MacroData), new MorphDesire(UnitTypes.TERRAN_ORBITALCOMMAND, 2, MacroData)));
-            // Supply depot, 19, 1:31
+            // Supply depot, 19
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(19, MacroData), new SupplyDepotDesire(1, MacroData)));
-            // Factory, 20, 1:42
+            // Factory, 20
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(20, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_FACTORY, 1, MacroData)));
-            // Reaper, 21, 2:03
+            // Reaper, 21
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(21, MacroData), new UnitDesire(UnitTypes.TERRAN_REAPER, 1, MacroData.DesiredUnitCounts)));
-            // Command center, 23, 2:21
+            // Command center, 23
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(23, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_COMMANDCENTER, 3, MacroData)));
-            // Hellion, 24, 2:28
+            // Hellion, 24
             BuildOrder.Enqueue(new BuildAction(new SupplyCondition(24, MacroData), new UnitDesire(UnitTypes.TERRAN_HELLION, 1, MacroData.DesiredUnitCounts)));
+            // StarPort, 24
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(24, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_STARPORT, 1, MacroData)));
+            // Barracks Reactor
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(24, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_BARRACKSREACTOR, 1, MacroData)));
+            // Supply Depot
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(24, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_SUPPLYDEPOT, 1, MacroData)));
+            // Widow Mine
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(24, MacroData), new ProductionStructureDesire(UnitTypes.TERRAN_WIDOWMINE, 1, MacroData)));
+            // Marine x2
+            BuildOrder.Enqueue(new BuildAction(new SupplyCondition(24, MacroData), new UnitDesire(UnitTypes.TERRAN_MARINE, 2, MacroData.DesiredUnitCounts)));
+            
         }
 
         private int FrameFromTime(int minutes, int seconds)
@@ -122,13 +94,13 @@ namespace StarCraft2Bot.Builds
             }
 
             var nextAction = BuildOrder.Peek();
-
+            
             if (nextAction.AreConditionsFulfilled())
             {
                 nextAction.EnforceDesires();
                 BuildOrder.Dequeue();
             }
-
+            
             ManageAttackCondition(observation);
         }
 
@@ -150,7 +122,6 @@ namespace StarCraft2Bot.Builds
             if (BuildOrder.Count == 0)
             {
                 AttackData.UseAttackDataManager = true;
-                proxyTask.Disable();
                 return true;
             }
 
