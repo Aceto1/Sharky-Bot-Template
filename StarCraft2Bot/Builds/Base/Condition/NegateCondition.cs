c840df122b805d399382d0521caa66604bed8b8d
Lucas Schumacher <lucas@aceto.dev>
2023-11-02 09:46:36 +0100

    EnemyHasUnitTypeCondition & NegateCondition

---
+++ StarCraft2Bot/Builds/Base/Condition/NegateCondition.cs
@@ -0,0 +1,17 @@
+﻿namespace StarCraft2Bot.Builds.Base.Condition
+{
+    public class NegateCondition : ICondition
+    {
+        public NegateCondition(ICondition condition)
+        {
+            Condition = condition;
+        }
+
+        public ICondition Condition { get; private set; }
+
+        public bool IsFulfilled()
+        {
+            return !Condition.IsFulfilled();
+        }
+    }
+}
